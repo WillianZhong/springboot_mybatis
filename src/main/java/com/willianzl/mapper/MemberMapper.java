6f25cfec28f228f7ace8d2328c9bd23751fcdf4f
WillianZhong <willianzhongzl@gmail.com>
2018-05-05 22:01:03 +0800

    Initial commit

---
+++ src/main/java/com/willianzl/mapper/MemberMapper.java
@@ -0,0 +1,17 @@
+package com.willianzl.mapper;
+
+import com.willianzl.model.Member;
+
+public interface MemberMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
+}
---
+++ src/main/java/com/willianzl/model/Member.java
@@ -0,0 +1,33 @@
+package com.willianzl.model;
+
+public class Member {
+    private Long id;
+
+    private String name;
+
+    private Byte sex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Byte getSex() {
+        return sex;
+    }
+
+    public void setSex(Byte sex) {
+        this.sex = sex;
+    }
+}
