22cee83205d03c95055bc81f7a0871d12a7dae9f
WillianZhong <willainZhongzl@gmail.com>
2018-05-10 09:58:45 +0800

    Druid+Mybatis+多数据源

--- src/main/java/com/willianzl/SpringbootMybatisApplication.java
+++ src/main/java/com/willianzl/SpringbootMybatisApplication.java
@@ -3,6 +3,7 @@ package com.willianzl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring Boot 应用的标识
 @SpringBootApplication
 public class SpringbootMybatisApplication {
 
--- src/main/java/com/willianzl/mapper/MemberMapper.java
+++ src/main/java/com/willianzl/mapper/MemberMapper.java
@@ -1,7 +1,11 @@
 package com.willianzl.mapper;
 
 import com.willianzl.model.Member;
+import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
+@Mapper
 public interface MemberMapper {
     int deleteByPrimaryKey(Long id);
 
@@ -14,4 +18,6 @@ public interface MemberMapper {
     int updateByPrimaryKeySelective(Member record);
 
     int updateByPrimaryKey(Member record);
+
+    List<Member> selectAll();
 }
